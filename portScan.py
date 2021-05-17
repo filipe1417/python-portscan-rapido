10b084d84887e6b17ae962ce173c251fa952cf13
filipe <filipe1417@gmail.com>
2021-05-17 08:30:10 -0300

    commit inicial

---
+++ portScan.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import sys, socket 
+
+host = socket.gethostbyname(sys.argv[1])
+portas = sys.argv[2]
+
+def portScan(host,porta):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    if s.connect_ex((host,int(porta))) == 0:
+        print("Porta " + porta + " aberta")
+
+for p in portas.split(","):
+    portScan(host,p)
