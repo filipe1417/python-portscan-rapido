568c2307524e8f0808e453abd585ed6b3f843c80
FIlipe <filipe1417@gmail.com>
2021-05-17 08:35:35 -0300

    Update portScan.py

--- portScan.py
+++ portScan.py
@@ -7,7 +7,7 @@ portas = sys.argv[2]
 
 def portScan(host,porta):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
+    s.settimeout(2)
     if s.connect_ex((host,int(porta))) == 0:
         print("Porta " + porta + " aberta")
 
