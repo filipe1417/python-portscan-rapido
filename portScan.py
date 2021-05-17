06c787d98f37ec0ba316802b1c0b5b3798b2a749
filipe <filipe1417@gmail.com>
2021-05-17 09:55:40 -0300

    comentarios adicionados

--- portScan.py
+++ portScan.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
 import sys, socket 
+#importação das bibliotecas utilizadas
 
 host = socket.gethostbyname(sys.argv[1])
-portas = sys.argv[2]
+#armazenamento do endereço IPV4 do host na variável "host". A função "gethostbyname()" garante que mesmo o usuário passando um nome de domínio, o valor armazenado será o endereço IP.
 
-def portScan(host,porta):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
-    if s.connect_ex((host,int(porta))) == 0:
-        print("Porta " + porta + " aberta")
+portas = sys.argv[2]
+#aqui são armazenadas as portas escolhidas pelo usuário.
 
 for p in portas.split(","):
-    portScan(host,p)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+    if s.connect_ex((host,int(p))) == 0:
+        print("Porta " + p + " aberta")
+#caso o usuário digite mais de uma porta, separando por vírgula, o "split(",")" vai garantir que a string seja transformada em valores distintos. Além disso, o "int(p)" garante que o valor da porta seja passada para inteiro e possa funcionar corretamente dentro da função "connect_ex()" 
