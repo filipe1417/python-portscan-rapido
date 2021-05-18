b9b5e496fd8a5705e31d4882cbbd466b8c211188
filipe <filipe1417@gmail.com>
2021-05-18 03:13:27 -0300

    função

--- portScan.py
+++ portScan.py
@@ -10,11 +10,14 @@ host = socket.gethostbyname(sys.argv[1])
 portas = sys.argv[2]
 #aqui são armazenadas as portas escolhidas pelo usuário.
 
-for p in portas.split(","):
+def portScan(host,porta):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
-    if s.connect_ex((host,int(p))) == 0:
-        print("Porta " + p + " aberta")
+    if s.connect_ex((host,int(porta))) == 0:
+        print("Porta " + porta + " aberta")
+
+for p in portas.split(","):
+    portScan(host,p)
 #caso o usuário digite mais de uma porta, separando por vírgula, o "split(",")" vai garantir -> 
 #-> que a string seja transformada em valores distintos
 #além disso, o "int(p)" garante que o valor da porta seja passada para inteiro e possa -> 
