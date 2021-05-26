586ff48a3fcc9107b04e1f8e10cd07432034472b
filipe <filipe1417@gmail.com>
2021-05-26 09:34:29 -0300

    range

--- portScan.py
+++ portScan.py
@@ -5,19 +5,23 @@ import sys, socket, multiprocessing, argparse
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-m","--mainports", help="Realiza um scan nas 100 principais portas", action="store_true")
-group.add_argument("-p","--ports", help="Especifique as portas, separando por virgula")
+group.add_argument("-p","--ports", help="Especifique as portas, separando por virgula ou hífen")
 parser.add_argument("ip",help="Digite o endereço ip ou domínio")
 args = parser.parse_args()
 
 def portScan(host, porta):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
-    if s.connect_ex((host,int(porta))) == 0:
-        print("Porta " + porta + " aberta")
+    try:
+        if s.connect_ex((host,int(porta))) == 0:
+            print("Porta " + porta + " aberta")
+    except ValueError:
+        print("Um erro ocorreu. Verificar se o modelo de digitação das portas confere com o existente no --help")
+        sys.exit()
 
 def multiProcess(host, portScan, p):
-    l = multiprocessing.Process(target=portScan, args=(host,str(p)))
-    l.start()
+        l = multiprocessing.Process(target=portScan, args=(host,str(p)))
+        l.start()
 
 def main():
     try:
@@ -33,9 +37,19 @@ def main():
 
     else:
         portas = args.ports
-        for p in portas.split(","):
-            multiProcess(host, portScan, p)
-
+        
+        try:
+            if "," in portas:
+                for p in portas.split(","):
+                    multiProcess(host, portScan, p)
+            elif "-" in portas:
+                portaInicial, portaFinal = portas.split("-")
+                for p in range (int(portaInicial), int(portaFinal)+1):
+                    multiProcess(host, portScan, p)
+            else:
+                print("Um erro ocorreu. Checar a digitação das portas")
+        except ValueError:
+            print("Um erro ocorreu. Verificar se o modelo de digitação das portas confere com o existente no --help")
+            
 if __name__ == "__main__":
     main()
-    
---
+++ teste.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+portas = "20-25"
+
+
+#portaInicial, portaFinal = portas.split("-")
+for p in range (int(portas.split("-"))):
+    print("a")    
