a37839fb9c41acca342d8b4032faacf3c72ffa54
filipe1417 <filipe1417@gmail.com>
2022-08-20 15:55:49 -0300

    formatação do código

--- portScan.py
+++ portScan.py
@@ -3,12 +3,7 @@
 import sys, socket, multiprocessing, argparse
 from constantes import portasPrincipais
 
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-m","--mainports", help="Realiza um scan nas 1000 principais portas tcp", action="store_true")
-group.add_argument("-p","--ports", help="Especifique as portas, separando por virgula ou hífen")
-parser.add_argument("ip",help="Digite o endereço ip ou domínio")
-args = parser.parse_args()
+
 
 def portScan(host, porta):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,11 +11,21 @@ def portScan(host, porta):
     if s.connect_ex((host,int(porta))) == 0:
         print("Porta " + porta + " aberta")
 
+
 def multiProcess(host, portScan, p):
         l = multiprocessing.Process(target=portScan, args=(host,str(p)))
         l.start()
 
+
 def main():
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-m","--mainports", help="Realiza um scan nas 1000 principais portas tcp", action="store_true")
+    group.add_argument("-p","--ports", help="Especifique as portas, separando por virgula ou hífen")
+    parser.add_argument("ip",help="Digite o endereço ip ou domínio")
+    args = parser.parse_args()
+
     try:
         host = socket.gethostbyname(args.ip)
     except socket.gaierror:
@@ -31,11 +36,11 @@ def main():
         portas = portasPrincipais
         for p in portas:
             multiProcess(host, portScan, p)
-
     else:
         portas = args.ports
         for p in portas.split(","):
             multiProcess(host, portScan, p)
-            
+
+
 if __name__ == "__main__":
     main()
